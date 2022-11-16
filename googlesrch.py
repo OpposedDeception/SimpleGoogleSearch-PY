55063a0f760878723c46410a2689222ec6fb9a76
KotenAngered <113610915+KotenAngered@users.noreply.github.com>
2022-11-16 21:28:41 +0100

    Update googlesrch.py

--- googlesrch.py
+++ googlesrch.py
@@ -5,12 +5,14 @@ try:
     from gsearchlib import Search
     from colorama import Fore, Back, init
     from time import sleep
+    import urllib.request
     import urllib
 except ImportError:
     import_error.append('gsearchlib')
     import_error.append('colorama')
     import_error.append('time')
     import_error.append('urllib3')
+    import_arror.append('urllib.request)
     user_choice = input("You got import error. Do you want to continue?: ")
 
     positive_variants = ['yes',
@@ -39,46 +41,26 @@ except ImportError:
 
 init(convert=True)
 
-while True:
-    print(Back.CYAN + Fore.BLACK + '1. Open link.')
-    print(Back.CYAN + Fore.BLACK + '2. Read URL data')
-    print(Back.CYAN + Fore.BLACK + '3. Google Search')
-
-    usrintinp = int(input(Back.CYAN + Fore.BLACK + 'Welcome to my program! \nWrite function you want to see: '))
-
-    usrfucntion = ('1', '2', '3')
-    if usrintinp in usrfucntion == 1:
-        openthis_url() # this function is not defined yet
-    elif usrintinp in usrfucntion == 2:
-        read_url_from_input() # this function is not defined yet
-    elif usrintinp in usrfucntion == 3:
-        google_search() # this function is not defined yet
-    else:
-        input("This function doesnt exist :( \nPress ENTER to close")
-        exit()
-
-
-
 def openthis_url(url):
-    get_url = urllib.urlopen(url)
-    print("Response Status: "+ str(get_url.getcode()) )
+    with urllib.request.urlopen(url) as get_url:
+        print(f'Response Status: {get_url.getcode}')
 def read_url_from_input(url_2):
     get_url_2 = urllib.urlopen(url_2)
     print(get_url_2.read())
 
 def openthe_link():
-        print("1. Get response status from the link. \n2. Read url file")
+        print("3. Get response status from the link. \n4. Read url file")
 
         usrinp = input("Tell me what to do: ")
-        choice_from_user = ('1', '2')
-        if usrinp in choice_from_user == 1:
+        choice_from_user = ('3', '4')
+        if usrinp in (choice_from_user):
             usrinpchoice = input("Paste your link here: ")
             result_from_1 = openthis_url(usrinpchoice)
             print(result_from_1)
             A = input("Are you happy with the result? ")
             B = input("Thank you for using my functions! \nPress ENTER to close")
             exit()
-        elif usrinp in choice_from_user == 2:
+        elif usrinp in (choice_from_user):
             usrinpchoice_2 = input("Paste your link here: ")
             result_from_2 = read_url_from_input(usrinpchoice_2)
             print(result_from_2)
@@ -103,3 +85,18 @@ def google_search():
             elif k not in (yes):
                 input("Thank you for using my little app! \nPress ENTER to close")
                 exit()
+
+while True:
+     print(Back.CYAN + Fore.BLACK + '1. More URL functions')
+     print(Back.CYAN + Fore.BLACK + '2. Google Search')
+
+     usrintinp = int(input(Back.CYAN + Fore.BLACK + 'Welcome to my program! \nWrite function you want to see: '))
+
+     usrfucntion = ('1', '2')
+     if usrintinp == 1:
+         openthe_link()
+     elif usrintinp == 2:
+         google_search()
+     else:
+         input("This function doesnt exist :( \nPress ENTER to close")
+         exit()
