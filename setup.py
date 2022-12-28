acaf9db70d1731582eda91e9259d8370c906abf6
KotenAngered <113610915+KotenAngered@users.noreply.github.com>
2022-12-29 00:51:46 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SimpleGoogleSearch-PY',
+    version='0.5Beta',
+    description='Simple google search engine in command prompt',
+    author='KotenAngered',
+    author_email='anonovezgame@tutanota.com',
+    url='https://https://github.com/KotenAngered/SimpleGoogleSearch-PY',
+    packages=find_packages(include=['googlesrch.py']),
+    install_requires=[
+        'gsearchlib',
+        'pprintpp',
+        'colorama',
+        'urllib.request',
+        'rich',
+        'webbrowser'
+    ],
+)
